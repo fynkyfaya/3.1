561bba8bfaa01fe7180704012b1237faa7b86d7a
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:25:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+double q = 0;
+for (var k = 1; k <= n; k++) {
+
+    var factorial = 1;
+    for (var x = 1; x <= k + n; x++) factorial *= x;
+    
+    var numerator = Math.Pow(-1, k) * (k - 7);
+    
+    var result = numerator / factorial;
+    if (result == 0 || double.IsInfinity(result)) continue;
+    
+    q += result;
+}
+
+Console.WriteLine(q.ToString("F5"));
